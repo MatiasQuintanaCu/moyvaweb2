eb831ac46eb1bb07bafeb0a80ce19b7faed51d86
matiasquintana <matiad12342@gmail.com>
2026-05-09 14:14:25 -0300

    sss

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Totem Games-Moyva-46.0";
+const contentToCache = [
+    "Build/htdocs.loader.js",
+    "Build/htdocs.framework.js.unityweb",
+    "Build/htdocs.data.unityweb",
+    "Build/htdocs.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
